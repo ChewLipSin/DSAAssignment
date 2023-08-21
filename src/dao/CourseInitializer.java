7c7295b4d6a16cfa6e4fe7ec0da17c4e7731d284
ChewLipSin <chewls-wm21@student.tarc.edu.my>
2023-08-22 00:36:03 +0800

    Fixed Quick Sort list bug and implements insertion sort

--- src/dao/CourseInitializer.java
+++ src/dao/CourseInitializer.java
@@ -22,11 +22,23 @@ public class CourseInitializer {
     public ListInterface<Course> initializeCourse() {
         ListInterface<Course> cList = new ArrList<>();
         cList.add(new Course("BAIT1013", "Introduction of Computer Networks", 4, Course.Sem.JAN));
-        cList.add(new Course("BAMS1623", "Discrete Mathematics", 3, Course.Sem.JAN));
-        cList.add(new Course("BAMS1054", "Probability and Statistics", 4, Course.Sem.JAN));
+        cList.add(new Course("BAMS1623", "Discrete Mathematics", 3, Course.Sem.ALL));
+        cList.add(new Course("BAMS1054", "Probability and Statistics", 4, Course.Sem.JUL));
         cList.add(new Course("BJEL1713", "English for Tertiary Studies", 3, Course.Sem.JAN));
-        cList.add(new Course("BJEL1723", "Academic English", 03, Course.Sem.JUL));
+        cList.add(new Course("BJEL1723", "Academic English", 3, Course.Sem.JUL));
         cList.add(new Course("BACS1053", "Database Management", 4, Course.Sem.ALL));
+        cList.add(new Course("BMCS2013", "Data Engineering", 4, Course.Sem.ALL));
+        cList.add(new Course("AACS1304", "System Analysis and Design", 3, Course.Sem.JUL));
+        cList.add(new Course("BAIT1043", "System Analysis and Design", 3, Course.Sem.JAN));
+        cList.add(new Course("AMIT1713", "IT Fundamentals and Applications", 3, Course.Sem.ALL));
+        cList.add(new Course("BMIT1723", "IT Fundamentals and Applications", 3, Course.Sem.ALL));
+        cList.add(new Course("ABFA1173", "Principles of Accounting ", 3, Course.Sem.ALL));
+        cList.add(new Course("BBFA1173", "Principles of Accounting ", 3, Course.Sem.ALL));
+        cList.add(new Course("BBMF3203", "Research Method", 3, Course.Sem.ALL));
+        cList.add(new Course("MPU-2312", "Civic Consciousness and Volunteerism", 2, Course.Sem.ALL));
+        cList.add(new Course("MPU-2212", "Bahasa Kebangsaan A", 2, Course.Sem.ALL));
+        
+
         return cList;
     }
 
@@ -35,6 +47,7 @@ public class CourseInitializer {
         ListInterface<Course> courseList = c.initializeCourse();
         CourseMaintenanceUI courseUI = new CourseMaintenanceUI();
         CourseMaintenance courseMain = new CourseMaintenance(courseList);
+        Sort s = new Sort();
         // To illustrate how to use the initializeProducts() method
         CourseDAO d = new CourseDAO();
         d.saveToFile(courseList);
@@ -42,13 +55,14 @@ public class CourseInitializer {
         System.out.println("\nCourse:\n" + courseList);
         System.out.println("\nCourse:\n" + courseList2);
         courseUI.listAllCourses(courseMain.getAllCourses(courseList));
-        CourseMaintenance cm = new CourseMaintenance(courseList);
-        Sort.quickSort(courseList, 1, courseList.size(),"semester");
-        for (int i = 1; i <= courseList.size(); i++) {
-            System.out.println(courseList.getEntry(i));
-        }
+        CourseMaintenance cm = new CourseMaintenance(courseList); 
+//        s.insertionSort(courseList,"courseCode");
+        Sort.quickSort(courseList, 1, courseList.size(), "title");
+        System.out.println("\nCourse:\n" + courseList);
+
         cm.runCourseMaintenance(courseList);
 
     }
+
 
 }
--- src/utility/Sort.java
+++ src/utility/Sort.java
@@ -13,6 +13,53 @@ import entity.Course;
  */
 public class Sort {
 
+    public void insertionSort(ListInterface<Course> arr, String val) {
+        int n = arr.size();
+        for (int i = 1; i < n; i++) {
+            Course key = arr.getEntry(i + 1);
+
+            int j = i - 1;
+            if (val == "courseCode") {
+                while (j >= 0 && arr.getEntry(j + 1).getCourseCode().compareTo(key.getCourseCode()) > 0) {
+                    arr.replace(j + 2, arr.getEntry(j + 1));
+                    j--;
+                }
+            }
+            if (val == "title") {
+                while (j >= 0 && arr.getEntry(j + 1).getTitle().compareTo(key.getTitle()) > 0) {
+                    arr.replace(j + 2, arr.getEntry(j + 1));
+                    j--;
+                }
+            }
+            if (val == "semester") {
+                while (j >= 0 && arr.getEntry(j + 1).getSemester().getString(arr.getEntry(j + 1).getSemester()).compareTo(key.getSemester().getString(key.getSemester())) > 0) {
+                    arr.replace(j + 2, arr.getEntry(j + 1));
+                    j--;
+                }
+            }
+            if (val == "creditHours") {
+                while (j >= 0 && arr.getEntry(j + 1).getCreditHours() > key.getCreditHours()) {
+                    arr.replace(j + 2, arr.getEntry(j + 1));
+                    j--;
+                }
+            }
+            if (val == "updatedAt") {
+                while (j >= 0 && arr.getEntry(j + 1).getUpdatedAt().compareTo(key.getUpdatedAt()) > 0) {
+                    arr.replace(j + 2, arr.getEntry(j + 1));
+                    j--;
+                }
+            }
+            if (val == "createdAt") {
+                while (j >= 0 && arr.getEntry(j + 1).getCreatedAt().compareTo(key.getCreatedAt()) > 0) {
+                    arr.replace(j + 2, arr.getEntry(j + 1));
+                    j--;
+                }
+            }
+            arr.replace(j + 2, key);
+
+        }
+    }
+
     public static void quickSort(ListInterface<Course> arr, int low, int high, String val) {
         if (low < high) {
             int partitionIndex = partition(arr, low, high, val);
@@ -32,37 +79,49 @@ public class Sort {
                     swap(arr, i, j);
                 }
             }
-        } else if (val == "title") {
+        }
+        if (val == "title") {
             for (int j = low; j < high; j++) {
                 if (arr.getEntry(j).getTitle().compareTo(pivot.getTitle()) < 0) {
                     i++;
                     swap(arr, i, j);
                 }
             }
-        } else if (val == "creditHours") {
+        }
+        if (val == "creditHours") {
             for (int j = low; j < high; j++) {
                 if (arr.getEntry(j).getCreditHours() < pivot.getCreditHours()) {
                     i++;
                     swap(arr, i, j);
                 }
             }
-        } else if (val == "timeCreated") {
-            for (int j = low; j < high; j++) {
-                if (arr.getEntry(j).getCreateTime().compareTo(pivot.getCreateTime()) < 0) {
-                    i++;
-                    swap(arr, i, j);
-                }
-            }
-        } else if (val == "updateCreated") {
-            for (int j = low; j < high; j++) {
-                if (arr.getEntry(j).getUpdateTime().compareTo(pivot.getUpdateTime()) < 0) {
-                    i++;
-                    swap(arr, i, j);
-                }
-            }
-        } else {
-            swap(arr, i + 1, high);
         }
+        if (val == "semester") {
+            for (int j = low; j < high; j++) {
+                if (arr.getEntry(j).getSemester().getString(arr.getEntry(j).getSemester()).compareTo(pivot.getSemester().getString(pivot.getSemester())) < 0)  {
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+        }
+        if (val == "createdAt") {
+            for (int j = low; j < high; j++) {
+                if (arr.getEntry(j).getCreatedAt().compareTo(pivot.getCreatedAt()) < 0) {
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+        }
+        if (val == "updatedAt") {
+            for (int j = low; j < high; j++) {
+                if (arr.getEntry(j).getUpdatedAt().compareTo(pivot.getUpdatedAt()) < 0) {
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+        }
+        swap(arr, i + 1, high);
+
         return i + 1;
     }
 
