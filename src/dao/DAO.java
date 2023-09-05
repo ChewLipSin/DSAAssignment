33b4a5910ce3e0ba54164c2a837e6fd177529efb
yi004J13 <140406283+yi004J13@users.noreply.github.com>
2023-09-05 11:33:49 +0800

    Remove  the tupid things (#22)

--- src/dao/DAO.java
+++ src/dao/DAO.java
@@ -1,9 +1,5 @@
 package dao;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 import adt.*;
 import java.io.*;
 import java.io.File;
